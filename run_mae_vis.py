c24f71764f53f1d6dd790c4737a9b70e71f23195
御坂17032号 <40137262+Misaka17032@users.noreply.github.com>
2023-07-25 14:19:23 +0800

    Update run_mae_vis.py

--- run_mae_vis.py
+++ run_mae_vis.py
@@ -117,13 +117,13 @@ def main(args):
         mask = torch.ones_like(img_patch)
         mask[bool_masked_pos] = 0
         mask = rearrange(mask, 'b n (p c) -> b n p c', c=3)
-        mask = rearrange(mask, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=14, w=14)
+        mask = rearrange(mask, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=args.window_size[0], w=args.window_size[1])
 
         #save reconstruction img
         rec_img = rearrange(img_patch, 'b n (p c) -> b n p c', c=3)
         # Notice: To visualize the reconstruction image, we add the predict and the original mean and var of each patch. Issue #40
         rec_img = rec_img * (img_squeeze.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6) + img_squeeze.mean(dim=-2, keepdim=True)
-        rec_img = rearrange(rec_img, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=14, w=14)
+        rec_img = rearrange(rec_img, 'b (h w) (p1 p2) c -> b c (h p1) (w p2)', p1=patch_size[0], p2=patch_size[1], h=args.window_size[0], w=args.window_size[1])
         img = ToPILImage()(rec_img[0, :].clip(0,0.996))
         img.save(f"{args.save_path}/rec_img.jpg")
 
